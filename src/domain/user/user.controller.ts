ab9e8481af757367552296445ea2cccea6f063fc
Padam7890 <padamthapamoho@gmail.com>
2024-10-14 19:39:09 +0545

    types fixed

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -13,53 +13,38 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiExcludeController, ApiExcludeEndpoint } from '@nestjs/swagger';
 
-//user controller 
+//user controller
 @Controller('user')
 @ApiExcludeController()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  //create user http method 
+  //create user http method
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
-    const createUser = await this.userService.create(createUserDto);
-    return {
-      statusCode: HttpStatus.CREATED,
-      message: 'User created successfully',
-      user: createUser,
-    };
+    return await this.userService.create(createUserDto);
   }
-  //get all user http method 
+  //get all user http method
   @Get()
   async findAll() {
-    const userAll=  await this.userService.findAll();
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'Users fetched successfully',
-      users: userAll,
-    }
+    return await this.userService.findAll();
   }
 
-  //get user by id http method 
+  //get user by id http method
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findoneByid(+id);
   }
 
-  //update user http method 
+  //update user http method
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
-  //delete user http method 
+  //delete user http method
   @Delete(':id')
   async remove(@Param('id') id: string) {
-   const deletedData =  await this.userService.remove(+id);
-   return {
-     statusCode: HttpStatus.OK,
-     message: 'User deleted successfully',
-     user: deletedData,
-   }
+    return await this.userService.remove(+id);
   }
 }
